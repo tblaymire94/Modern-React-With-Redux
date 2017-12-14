bb422f6c37133a1b69bd9ec589b08b810300ed44
Thomas Blaymire <thomas.blaymire@hermes-europe.co.uk>
2017-12-14 13:20:32 +0000

    Deployment To Heroku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -30,6 +31,7 @@ class App extends Component {
   }
 
   render() {
+
     return (
       <div>
         <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
